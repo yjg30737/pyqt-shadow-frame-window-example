b653d39c43b3397ec1f79eb8e5a1574ebe9a0531
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-27 11:46:02 +0900

    Update description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt shadow frame window example',
+    description='PyQt shadow(+rounded) frame window example',
     url='https://github.com/yjg30737/pyqt-shadow-frame-window-example.git',
     install_requires=[
         'PyQt5>=5.8'
