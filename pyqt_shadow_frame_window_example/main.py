19e433e3644903b4db11d54e10c620d35d02ca64
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-27 11:38:02 +0900

    Make frame rounded

--- pyqt_shadow_frame_window_example/main.py
+++ pyqt_shadow_frame_window_example/main.py
@@ -1,39 +1,64 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor
+from PyQt5.QtCore import Qt, QRectF, QMargins
+from PyQt5.QtGui import QColor, QPainter, QPainterPath, QBrush, QPen, QRegion, QPalette
 from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QGraphicsDropShadowEffect, QGridLayout, QLabel, \
     QTextEdit, QVBoxLayout
 
 
+# shadow + rounded frame
 class ShadowFrame(QWidget):
     def __init__(self, main_window):
         super().__init__()
+        # main_window is main widget to show
         self.__main_window = main_window
         self.__initUi()
 
     def __initUi(self):
+        # for frameless window/prevent occurrence of minor bugs
         self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowMinMaxButtonsHint)
-        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        # make basic background(which is dark) invisible to show shadow
+        self.setAttribute(Qt.WA_TranslucentBackground)
 
+        # set the shadow
         self.__effect = QGraphicsDropShadowEffect()
-        self.__effect.setBlurRadius(8.0)
+        self.__effect.setBlurRadius(12.0)
         self.__effect.setColor(QColor(0, 0, 0, 127))
         self.__effect.setOffset(0.0)
-        self.__main_window.setGraphicsEffect(self.__effect)
+        self.setGraphicsEffect(self.__effect)
 
         lay = QGridLayout()
         lay.addWidget(self.__main_window)
-        lay.setContentsMargins(2, 2, 2, 2)
         self.setLayout(lay)
 
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        # set the background and pen (looks like Windows 11 default window)
+        pen = QPen(QColor(Qt.gray), 1)
+        brush = QBrush(QColor(241, 241, 241, 255))
+        painter.setPen(pen)
+        painter.setBrush(brush)
 
+        # set rounded window
+        path = QPainterPath()
+        rect = self.rect()
+        # set margin to each direction for giving the space to show shadow
+        rect = rect.marginsRemoved(QMargins(5, 5, 5, 5))
+        # rounded corner's radius
+        radius = 12.0
+        path.addRoundedRect(QRectF(rect), radius, radius)
+        # set antialiasing to enhance quality of window
+        painter.setRenderHints(QPainter.Antialiasing)
+        painter.drawPath(path)
+
+        return super().paintEvent(e)
+
+
+# sample widget to put inside the shadow(+rounded) frame
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.__initUi()
 
     def __initUi(self):
-        self.setAutoFillBackground(True)
-
         lbl = QLabel()
         lbl.setText('Shadow window example')
         textEdit = QTextEdit()
@@ -45,3 +70,14 @@ class MainWindow(QMainWindow):
         mainWidget = QWidget()
         mainWidget.setLayout(lay)
         self.setCentralWidget(mainWidget)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    # put inside the shadow(+rounded) frame
+    window = ShadowFrame(widget)
+    window.show()
+    app.exec_()
