4bbbadfdad6e0cb87f9d01582ffaf86fe224a2b9
Jung Gyu Yoon <yjg30737@gmail.com>
2022-08-06 11:33:43 +0900

    Modify requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='PyQt shadow(+rounded) frame window example',
     url='https://github.com/yjg30737/pyqt-shadow-frame-window-example.git',
     install_requires=[
-        'PyQt5>=5.8',
+        'PyQt5>=5.15',
         'pyqt-frameless-window>=0.0.1'
     ]
 )
