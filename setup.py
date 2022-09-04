bd2b7e6860ea526949c701cdf9752b39ab294fbb
Jung Gyu Yoon <yjg30737@gmail.com>
2022-09-04 17:05:17 +0900

    Bump ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-shadow-frame-window-example',
-    version='0.0.3',
+    version='0.0.31',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
